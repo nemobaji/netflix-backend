4c0218b6c392522e1db5ec751cc6945c42366647
HyunwooKim04 <khw175@naver.com>
2026-09-18 19:01:05 +0900

    feat: add login api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { User } from '../user/entity/user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
@@ -11,6 +12,7 @@ export class AuthService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
     private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
   ) {}
 
   parseBasicToken(rawToken: string) {
@@ -52,5 +54,49 @@ export class AuthService {
     });
 
     return this.userRepository.findOne({ where: { email } });
+  }
+
+  async login(rawToken: string) {
+    const { email, password } = this.parseBasicToken(rawToken);
+    const user = await this.userRepository.findOne({ where: { email } });
+    if (!user) {
+      throw new BadRequestException('잘못된 로그인 정보입니다.');
+    }
+    const passOk = await bcrypt.compare(password, user.password);
+    if (!passOk) {
+      throw new BadRequestException('잘못된 로그인 정보입니다.');
+    }
+
+    const refreshTokenSecret = this.configService.getOrThrow<string>(
+      'REFRESH_TOKEN_SECRET',
+    );
+    const accessTokenSecret = this.configService.getOrThrow<string>(
+      'ACCESS_TOKEN_SECRET',
+    );
+
+    return {
+      refreshToken: await this.jwtService.signAsync(
+        {
+          sub: user.id,
+          role: user.role,
+          type: 'refresh',
+        },
+        {
+          secret: refreshTokenSecret,
+          expiresIn: '24h',
+        },
+      ),
+      accessToken: await this.jwtService.signAsync(
+        {
+          sub: user.id,
+          role: user.role,
+          type: 'access',
+        },
+        {
+          secret: accessTokenSecret,
+          expiresIn: 300,
+        },
+      ),
+    };
   }
 }
